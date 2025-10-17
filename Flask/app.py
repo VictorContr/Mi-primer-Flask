3c60e3517feea8776d3c8623e58ad4033eb162eb
VictorContr <contrerasv174@gmail.com>
2025-10-16 21:13:58 -0400

    Mejora del sitio web y ahora es de la UNE

--- Flask/app.py
+++ Flask/app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("home.html")
+def index():
+    return render_template("index.html")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+@app.route('/carreras/')
+def carreras():
+    return render_template("carreras.html")
 
 if __name__ == '__main__':
     app.run(debug= False)
